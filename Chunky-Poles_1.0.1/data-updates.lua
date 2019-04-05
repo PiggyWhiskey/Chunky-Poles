e9d842507330b72c51c7a31922cba4bddc1748e7
PiggyWhiskey <bromy2004@gmail.com>
2019-04-05 16:08:25 +0930

    Proper Bobs Power compatibility

--- Chunky-Poles_1.0.1/data-updates.lua
+++ Chunky-Poles_1.0.1/data-updates.lua
@@ -12,23 +12,26 @@ end
 
 --Big Pole to fit Chunks (Capped at 64)
 data.raw["electric-pole"]["big-electric-pole"].maximum_wire_distance = 32 --1 Chunk
-if settings.startup["bobmods-power-poles"].value == true then
-	data.raw["electric-pole"]["big-electric-pole-2"].maximum_wire_distance = 40 --1.25 Chunk
-	data.raw["electric-pole"]["big-electric-pole-3"].maximum_wire_distance = 48 --1.5 Chunk
-	data.raw["electric-pole"]["big-electric-pole-4"].maximum_wire_distance = 64 --2 Chunk
+if mods[bobpower] then
+	if settings.startup["bobmods-power-poles"].value == true then
+		data.raw["electric-pole"]["big-electric-pole-2"].maximum_wire_distance = 40 --1.25 Chunk
+		data.raw["electric-pole"]["big-electric-pole-3"].maximum_wire_distance = 48 --1.5 Chunk
+		data.raw["electric-pole"]["big-electric-pole-4"].maximum_wire_distance = 64 --2 Chunk
+	end
 end
-
 
 --Substation to fit Chunks
 data.raw["electric-pole"]["substation"].maximum_wire_distance = 16 --0.5 Chunk
 data.raw["electric-pole"]["substation"].supply_area_distance = data.raw["electric-pole"]["substation"].maximum_wire_distance / 2
-if settings.startup["bobmods-power-poles"].value == true then
-	data.raw["electric-pole"]["substation-2"].maximum_wire_distance = 24 --0.75 Chunk
-	data.raw["electric-pole"]["substation-3"].maximum_wire_distance = 32 --1 Chunk
-	data.raw["electric-pole"]["substation-4"].maximum_wire_distance = 40 --1.25 Chunk
-	
-	data.raw["electric-pole"]["substation-2"].supply_area_distance = data.raw["electric-pole"]["substation-2"].maximum_wire_distance / 2
-	data.raw["electric-pole"]["substation-3"].supply_area_distance = data.raw["electric-pole"]["substation-3"].maximum_wire_distance / 2
-	data.raw["electric-pole"]["substation-4"].supply_area_distance = data.raw["electric-pole"]["substation-4"].maximum_wire_distance / 2
+if mods[bobpower] then
+	if settings.startup["bobmods-power-poles"].value == true then
+		data.raw["electric-pole"]["substation-2"].maximum_wire_distance = 24 --0.75 Chunk
+		data.raw["electric-pole"]["substation-3"].maximum_wire_distance = 32 --1 Chunk
+		data.raw["electric-pole"]["substation-4"].maximum_wire_distance = 40 --1.25 Chunk
+		
+		data.raw["electric-pole"]["substation-2"].supply_area_distance = data.raw["electric-pole"]["substation-2"].maximum_wire_distance / 2
+		data.raw["electric-pole"]["substation-3"].supply_area_distance = data.raw["electric-pole"]["substation-3"].maximum_wire_distance / 2
+		data.raw["electric-pole"]["substation-4"].supply_area_distance = data.raw["electric-pole"]["substation-4"].maximum_wire_distance / 2
 
+	end
 end
