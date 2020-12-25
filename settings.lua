7fc0847ac044e93a56ddd3241d0cb423c81acfe7
deimosian <deimosian@gmail.com>
2020-12-25 02:58:16 -0500

    Add files via upload

---
+++ settings.lua
@@ -0,0 +1,32 @@
+data:extend({
+    {
+        type = "bool-setting",
+        name = "piggy-small-pole",
+        setting_type = "startup",
+        default_value = true,
+    },
+    {
+        type = "bool-setting",
+        name = "piggy-medium-pole",
+        setting_type = "startup",
+        default_value = true,
+    },
+    {
+        type = "bool-setting",
+        name = "piggy-big-pole",
+        setting_type = "startup",
+        default_value = true,
+    },
+    {
+        type = "bool-setting",
+        name = "piggy-substation",
+        setting_type = "startup",
+        default_value = true,
+    },
+    {
+        type = "bool-setting",
+        name = "piggy-roboport",
+        setting_type = "startup",
+        default_value = true
+    }
+})
